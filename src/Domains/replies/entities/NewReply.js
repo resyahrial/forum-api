a96db88e328d529a6cdb1493195692dcc5837d74
resyahrial <syahrialrangga11@gmail.com>
2021-09-18 16:39:03 +0700

    Create new reply entity

--- src/Domains/replies/entities/NewReply.js
+++ src/Domains/replies/entities/NewReply.js
@@ -9,17 +9,18 @@ class NewComment {
   }
 
   _verifyPayload(payload) {
-    // const { content, threadId, owner } = payload;
-    // if (!content || !threadId || !owner) {
-    //   throw new Error('NEW_COMMENT.NOT_CONTAIN_NEEDED_PROPERTY');
-    // }
-    // if (
-    //   typeof content !== 'string' ||
-    //   typeof threadId !== 'string' ||
-    //   typeof owner !== 'string'
-    // ) {
-    //   throw new Error('NEW_COMMENT.NOT_MEET_DATA_TYPE_SPECIFICATION');
-    // }
+    const { content, threadId, commentId, owner } = payload;
+    if (!content || !threadId || !commentId || !owner) {
+      throw new Error('NEW_REPLY.NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+    if (
+      typeof content !== 'string' ||
+      typeof threadId !== 'string' ||
+      typeof commentId !== 'string' ||
+      typeof owner !== 'string'
+    ) {
+      throw new Error('NEW_REPLY.NOT_MEET_DATA_TYPE_SPECIFICATION');
+    }
   }
 }
 

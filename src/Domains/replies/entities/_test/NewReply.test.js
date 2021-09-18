89a6a7827720f8362c72583864b85aa1f96b7e07
Rangga Eki Syahrial <58381367+resyahrial@users.noreply.github.com>
2021-09-18 16:41:44 +0700

    Create new reply entity (#2)
    * Create test case for new reply entity
    
    * Create new reply entity

---
+++ src/Domains/replies/entities/_test/NewReply.test.js
@@ -0,0 +1,48 @@
+const NewReply = require('../NewReply');
+
+describe('NewReply entities', () => {
+  it('should throw error when payload does not contain needed property', () => {
+    const payload = {
+      title: 'New Reply',
+    };
+
+    expect(() => new NewReply(payload)).toThrowError(
+      'NEW_REPLY.NOT_CONTAIN_NEEDED_PROPERTY'
+    );
+  });
+
+  it('should throw error when payload not meet data type specification', () => {
+    // Arrange
+    const payload = {
+      content: 123456,
+      threadId: 'thread-qwerty',
+      commentId: 'comment-qwerty',
+      owner: 'user-qwerty',
+    };
+
+    // Action & Assert
+    expect(() => new NewReply(payload)).toThrowError(
+      'NEW_REPLY.NOT_MEET_DATA_TYPE_SPECIFICATION'
+    );
+  });
+
+  it('should create NewReply entities correctly', () => {
+    // Arrange
+    const payload = {
+      content: 'I think you need gacha',
+      threadId: 'thread-qwerty',
+      commentId: 'comment-qwerty',
+      owner: 'user-qwerty',
+    };
+
+    // Action
+    const newReply = new NewReply(payload);
+
+    // Assert
+    expect(newReply).toBeInstanceOf(NewReply);
+    expect(newReply.content).toEqual(payload.content);
+    expect(newReply.threadId).toEqual(payload.threadId);
+    expect(newReply.commentId).toEqual(payload.commentId);
+    expect(newReply.owner).toEqual(payload.owner);
+  });
+});
